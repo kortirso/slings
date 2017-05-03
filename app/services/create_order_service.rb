060ca13ff394b59e67fa025b931fec2331ed4f55
kortirso <kortirso@gmail.com>
2017-05-03 09:20:04 +0300

    fix

--- app/services/create_order_service.rb
+++ app/services/create_order_service.rb
@@ -1,5 +1,6 @@
 class CreateOrderService
     def self.call(params)
+        Order.where(cart_id: params[:order][:cart_id]).destroy_all
         order = Order.create params[:order]
         return nil if order.nil?
         delivery = Delivery.create params[:delivery].merge(order_id: order.id)
