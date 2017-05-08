aee4460cec7d94003e706289cc659468915f4fe9
kortirso <kortirso@gmail.com>
2017-05-08 11:28:36 +0300

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,59 +1,51 @@
 if Category.count == 0
-    category = Category.new name: 'Эрго-рюкзак', caption: 'Примерное описание категории'
+    category = Category.new name: 'Эрго-рюкзаки', caption: ''
     File.open("#{Rails.root}/db/category_images/nemo.png") { |f| category.image = f }
     category.save
 
-    category = Category.new name: 'Май-слинг', caption: 'Примерное описание категории'
+    category = Category.new name: 'Май-слинги', caption: ''
     File.open("#{Rails.root}/db/category_images/mai-sling.png") { |f| category.image = f }
     category.save
 
-    category = Category.new name: 'Слинг с кольцами', caption: 'Примерное описание категории'
+    category = Category.new name: 'Слинги с кольцами', caption: ''
     File.open("#{Rails.root}/db/category_images/with_rings.png") { |f| category.image = f }
     category.save
 
-    category = Category.new name: 'Шарф-рюкзак', caption: 'Примерное описание категории'
+    category = Category.new name: 'Шарф-рюкзаки', caption: ''
     File.open("#{Rails.root}/db/category_images/grey.png") { |f| category.image = f }
     category.save
 
-    category = Category.new name: 'Слинг-шарф', caption: 'Примерное описание категории'
+    category = Category.new name: 'Слинг-шарфы', caption: ''
     File.open("#{Rails.root}/db/category_images/bordo.png") { |f| category.image = f }
     category.save
 
-    category = Category.new name: 'Эрго-рюкзак для двойни', caption: 'Примерное описание категории'
+    category = Category.new name: 'Эрго-рюкзаки для двойни', caption: ''
     File.open("#{Rails.root}/db/category_images/twins.png") { |f| category.image = f }
-    category.save
-
-    category = Category.new name: 'Аксессуары', caption: 'Примерное описание категории'
-    File.open("#{Rails.root}/db/category_images/accessuar.png") { |f| category.image = f }
     category.save
 end
 
 if Product.count == 0
-    product = Product.new name: 'Яркие цветы', price: 3400, amount: 0, category: Category.find_by(name: 'Эрго-рюкзак')
+    product = Product.new name: 'Яркие цветы', price: 3400, amount: 0, category: Category.find_by(name: 'Эрго-рюкзаки')
     File.open("#{Rails.root}/db/slings_images/bright_flowers.png") { |f| product.image = f }
     product.save
 
-    product = Product.new name: 'Голубой спорт', price: 2600, amount: 0, category: Category.find_by(name: 'Май-слинг')
+    product = Product.new name: 'Голубой спорт', price: 2600, amount: 0, category: Category.find_by(name: 'Май-слинги')
     File.open("#{Rails.root}/db/slings_images/blue_sport.png") { |f| product.image = f }
     product.save
 
-    product = Product.new name: 'Натур', price: 2200, amount: 0, category: Category.find_by(name: 'Слинг с кольцами')
+    product = Product.new name: 'Натур', price: 2200, amount: 0, category: Category.find_by(name: 'Слинги с кольцами')
     File.open("#{Rails.root}/db/slings_images/natur.png") { |f| product.image = f }
     product.save
 
-    product = Product.new name: 'Серый', price: 3500, amount: 0, category: Category.find_by(name: 'Шарф-рюкзак')
+    product = Product.new name: 'Серый', price: 3500, amount: 0, category: Category.find_by(name: 'Шарф-рюкзаки')
     File.open("#{Rails.root}/db/slings_images/grey.png") { |f| product.image = f }
     product.save
 
-    product = Product.new name: 'Бордо', price: 3400, amount: 0, category: Category.find_by(name: 'Слинг-шарф')
+    product = Product.new name: 'Бордо', price: 3400, amount: 0, category: Category.find_by(name: 'Слинг-шарфы')
     File.open("#{Rails.root}/db/slings_images/bordo.png") { |f| product.image = f }
     product.save
 
-    product = Product.new name: 'Для двойни', price: 3300, amount: 0, category: Category.find_by(name: 'Эрго-рюкзак для двойни')
+    product = Product.new name: 'Для двойни', price: 3300, amount: 0, category: Category.find_by(name: 'Эрго-рюкзаки для двойни')
     File.open("#{Rails.root}/db/slings_images/twins.png") { |f| product.image = f }
-    product.save
-
-    product = Product.new name: 'Слингобусы "Морские"', price: 620, amount: 0, category: Category.find_by(name: 'Аксессуары')
-    File.open("#{Rails.root}/db/slings_images/sea.png") { |f| product.image = f }
     product.save
 end
