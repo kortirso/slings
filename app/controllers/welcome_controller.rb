3e253eb5e7b40d43fa69539c42aca77efcb7597f
kortirso <kortirso@gmail.com>
2017-05-12 16:24:14 +0300

    remove category from index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController    
     def index
-        @categories = Category.where.not(name: 'Слинги с кольцами').order(name: :asc)
+        @categories = Category.where('name != ? AND name != ?', 'Слинги с кольцами', 'Ткани').order(name: :asc)
         @articles = Article.order(id: :desc).limit(3)
     end
 end
