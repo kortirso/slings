8c2877e75a4fab0efb6a40c79c3fd29e4507face
kortirso <kortirso@gmail.com>
2017-12-07 07:51:53 +0300

    fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -38,9 +38,8 @@ set :stage, :production
 #
 # Global options
 # --------------
-set :ssh_options, [
+set :ssh_options,
     keys: %w[~/.ssh/id_rsa],
     forward_agent: true,
     auth_methods: %w[publickey password],
     port: 2999
-]
