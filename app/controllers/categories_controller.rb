de635585e9ac108dc0d08cc0c4fb4ed8dffea90e
kortirso <kortirso@gmail.com>
2017-12-26 14:48:35 +0300

    add component for rendering novelty products

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,28 +1,43 @@
 class CategoriesController < ApplicationController
     before_action :categories_list
     before_action :find_category, only: %i[show]
+    before_action :find_novelty_products, only: %i[index]
+    before_action :find_category_products, only: %i[show]
 
     def index
-        @new_ones = Product.lasts.limit(8)
+        render_page
     end
 
     def show
+        render_page
+    end
+
+    private
+
+    def render_page
         respond_to do |format|
-            format.html
+            format.html do
+                render action_name
+            end
             format.json do
-                products = @category.products.lasts.page(select_page).per(8)
                 render json: {
-                    products: ActiveModel::Serializer::CollectionSerializer.new(products, each_serializer: ProductSerializer),
-                    next_page: products.next_page
+                    products: ActiveModel::Serializer::CollectionSerializer.new(@products, each_serializer: ProductSerializer),
+                    next_page: @products.next_page
                 }, status: 200
             end
         end
     end
 
-    private
-
     def find_category
         @category = Category.friendly.find(params[:id])
+    end
+
+    def find_novelty_products
+        @products = Product.lasts.page(select_page).per(8)
+    end
+
+    def find_category_products
+        @products = @category.products.lasts.page(select_page).per(8)
     end
 
     def select_page
---
+++ app/javascript/packs/novelty_box.jsx
@@ -0,0 +1,4 @@
+import NoveltyBox from 'components/novelty_box/novelty_box';
+import WebpackerReact from 'webpacker-react';
+
+WebpackerReact.setup({NoveltyBox});
