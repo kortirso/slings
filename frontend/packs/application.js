c28ac98891fc5a5f6c42a64e2a3cc199752ff315
kortirso <kortirso@gmail.com>
2018-01-22 11:45:54 +0300

    add metrics component

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -5,6 +5,7 @@ import "components/page/page";
 import "components/header/header";
 import "components/navigation/navigation";
 import "components/footer/footer";
+import "components/metrics/metrics";
 import "components/cart_preview/cart_preview";
 
 import "components/welcome/welcome";
