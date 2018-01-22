37ac11e47b78990381bde2ad1d559650e3524870
kortirso <kortirso@gmail.com>
2018-01-22 11:15:47 +0300

    add footer component

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -4,5 +4,6 @@ import "images/image";
 import "components/page/page";
 import "components/header/header";
 import "components/navigation/navigation";
+import "components/footer/footer";
 
 import "components/welcome/welcome";
