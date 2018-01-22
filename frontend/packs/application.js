96018e4b73a65353a246b448888b2f1a75851fce
kortirso <kortirso@gmail.com>
2018-01-22 11:19:21 +0300

    add cart_preview component

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -5,5 +5,6 @@ import "components/page/page";
 import "components/header/header";
 import "components/navigation/navigation";
 import "components/footer/footer";
+import "components/cart_preview/cart_preview";
 
 import "components/welcome/welcome";
