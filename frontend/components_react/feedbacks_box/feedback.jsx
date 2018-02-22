70ef50020b5b34c681a56d6d8ff3aa483b2ad04b
kortirso <kortirso@gmail.com>
2018-02-22 10:17:22 +0700

    modify feedback block

--- frontend/components_react/feedbacks_box/feedback.jsx
+++ frontend/components_react/feedbacks_box/feedback.jsx
@@ -3,19 +3,28 @@ import defaultImage from 'images/profile.jpg';
 
 export default class Feedback extends React.Component {
     _prepareImage(feedback) {
-        if(feedback.image != '') return <img src={feedback.image} alt={'Фото от ' + feedback.name} />;
+        if(feedback.image != '') return <img src={feedback.image} alt={'Фото от ' + feedback.name} />
         return <img src={defaultImage} alt='empty image' className='empty' />
     }
 
+    _prepareBody(feedback) {
+        return (
+            <div className='review_text'>
+                <p>{feedback.short_body}</p>
+                {feedback.body != feedback.short_body &&
+                    <p className='link_reviews'><a href='/reviews'>Читать далее</a></p>
+                }
+            </div>
+        )
+    }
+
     render() {
-        let feedback = this.props.feedback;
+        let feedback = this.props.feedback
         return (
             <div className='cell small-12 medium-6'>
                 <div className='review_block'>
                     <div className='review_icon'></div>
-                    <div className='review_text'>
-                        <p>{feedback.body}</p>
-                    </div>
+                    {this._prepareBody(feedback)}
                     <div className='review_author'>
                         <div className='author_name'>{feedback.name}</div>
                         <div className='author_image'>
@@ -24,6 +33,6 @@ export default class Feedback extends React.Component {
                     </div>
                 </div>
             </div>
-        );
+        )
     }
 }
