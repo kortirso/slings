3594a811165bfb7b3180373611fd3c2dc6d63559
kortirso <kortirso@gmail.com>
2018-03-15 12:18:58 +0700

    add filters buttons and actions for products

--- frontend/components_react/novelty_box/novelty_box.jsx
+++ frontend/components_react/novelty_box/novelty_box.jsx
@@ -9,6 +9,9 @@ export default class NoveltyBox extends React.Component {
         this.state = {
             categoryName: 'Новинки',
             productsList: [],
+            filteredList: [],
+            sort: 'none',
+            materials: '0',
             nextPage: 1
         }
     }
@@ -22,13 +25,17 @@ export default class NoveltyBox extends React.Component {
             method: 'GET',
             url: `categories.json?page=${this.state.nextPage}`,
             success: (data) => {
-                this.setState({productsList: this.state.productsList.concat(data.products), nextPage: data.next_page});
+                this.setState({productsList: this.state.productsList.concat(data.products), filteredList: this._filterList(this.state.productsList.concat(data.products)), nextPage: data.next_page});
             }
         });
     }
 
+    _filterList(products) {
+        return products
+    }
+
     _prepareProductsList() {
-        return this.state.productsList.map((product) => {
+        return this.state.filteredList.map((product) => {
             return (
                 <Product product={product} key={product.id} />
             );
@@ -39,11 +46,32 @@ export default class NoveltyBox extends React.Component {
         if(this.state.nextPage != null) return <a className='button' onClick={this._fetchProductsList.bind(this)}>Показать больше товаров</a>;
     }
 
+    changeMaterial(event) {
+        this.setState({material: event.target.value})
+    }
+
+    changeSort(sort) {
+        this.setState({sort: sort})
+    }
+
     render() {
         if(this.state.productsList.length == 0) return <h2>В данной категории нет товаров</h2>;
         return (
             <div>
                 <h2>{this.state.categoryName}</h2>
+                <div className='filters'>
+                    <div className='filter'>
+                        <span>Сортировка по цене</span><a onClick={this.changeSort.bind(this, 'up')}>&#8593;</a><a onClick={this.changeSort.bind(this, 'down')}>&#8595;</a>
+                    </div>
+                    <div className='filter'>
+                        <span>Материал</span>
+                        <select onChange={this.changeMaterial.bind(this)}>
+                            <option value='0'>Все</option>
+                            <option value='Хлопок'>Хлопок</option>
+                            <option value='Лён'>Лён</option>
+                        </select>
+                    </div>
+                </div>
                 <div className='catalog'>
                     <div className='products grid-x'>
                         {this._prepareProductsList()}
--- frontend/components_react/products_box/products_box.jsx
+++ frontend/components_react/products_box/products_box.jsx
@@ -10,6 +10,9 @@ export default class ProductsBox extends React.Component {
             categorySlug: props.category.slug,
             categoryName: props.category.name,
             productsList: [],
+            filteredList: [],
+            sort: 'none',
+            materials: '0',
             nextPage: 1
         }
     }
@@ -23,13 +26,17 @@ export default class ProductsBox extends React.Component {
             method: 'GET',
             url: `${this.state.categorySlug}.json?page=${this.state.nextPage}`,
             success: (data) => {
-                this.setState({productsList: this.state.productsList.concat(data.products), nextPage: data.next_page});
+                this.setState({productsList: this.state.productsList.concat(data.products), filteredList: this._filterList(this.state.productsList.concat(data.products)), nextPage: data.next_page});
             }
         });
     }
 
+    _filterList(products) {
+        return products
+    }
+
     _prepareProductsList() {
-        return this.state.productsList.map((product) => {
+        return this.state.filteredList.map((product) => {
             return (
                 <Product product={product} key={product.id} />
             );
@@ -40,11 +47,32 @@ export default class ProductsBox extends React.Component {
         if(this.state.nextPage != null) return <a className='button' onClick={this._fetchProductsList.bind(this)}>Показать больше товаров</a>;
     }
 
+    changeMaterial(event) {
+        this.setState({material: event.target.value})
+    }
+
+    changeSort(sort) {
+        this.setState({sort: sort})
+    }
+
     render() {
         if(this.state.productsList.length == 0) return <h2>В данной категории нет товаров</h2>;
         return (
             <div>
                 <h2>{this.state.categoryName}</h2>
+                <div className='filters'>
+                    <div className='filter'>
+                        <span>Сортировка по цене</span><a onClick={this.changeSort.bind(this, 'up')}>&#8593;</a><a onClick={this.changeSort.bind(this, 'down')}>&#8595;</a>
+                    </div>
+                    <div className='filter'>
+                        <span>Материал</span>
+                        <select onChange={this.changeMaterial.bind(this)}>
+                            <option value='0'>Все</option>
+                            <option value='Хлопок'>Хлопок</option>
+                            <option value='Лён'>Лён</option>
+                        </select>
+                    </div>
+                </div>
                 <div className='catalog'>
                     <div className='products grid-x'>
                         {this._prepareProductsList()}
