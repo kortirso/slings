55ed0583a4a735817fab38b67ff95d47d3e823f9
kortirso <kortirso@gmail.com>
2018-04-28 21:37:14 +0700

    modify instagram block

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,7 @@ class WelcomeController < ApplicationController
 
   def index
     @articles = Article.order(id: :desc).limit(3)
-    @insta_photos = Instagram.last.try(:insta_photos)
+    @insta_photos = Instagram.last.instaphotos.order(id: :asc).limit(9)
     @last_products = Product.lasts.limit(4)
   end
 end
--- app/models/instagram.rb
+++ app/models/instagram.rb
@@ -4,15 +4,6 @@ class Instagram < ApplicationRecord
 
   after_commit :create_photos, on: :create
 
-  def insta_photos
-    photos = instaphotos.to_a
-    [
-      [photos[0], photos[1], photos[2]],
-      [photos[3], photos[4], photos[5]],
-      [photos[6], photos[7], photos[8]]
-    ]
-  end
-
   private def create_photos
     return if Rails.env.test?
     elements = InstagramService.new.insta_photos
